93ae03c8d9d52087f5bbcb3d9ea77cecf02c33ed
Dana Hedrick <dana.marie.hedrick@gmail.com>
2019-10-07 16:20:26 -0700

    created view artist, and review model

--- src/main/java/com/music_map/controllers/ApiController.java
+++ src/main/java/com/music_map/controllers/ApiController.java
@@ -2,11 +2,15 @@ package com.music_map.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.music_map.models.Review;
 import com.music_map.services.ApiService;
+import com.wrapper.spotify.model_objects.specification.AlbumSimplified;
 import com.wrapper.spotify.model_objects.specification.Artist;
 import com.wrapper.spotify.model_objects.specification.Paging;
 
@@ -30,9 +34,14 @@ public String findArtists(Model model, @RequestParam(value="search", required=fa
 	model.addAttribute("search", artist);
 	return "search.jsp";
 }
-@RequestMapping("/view/{id}")
-public String viewArtist(Model model, @PathVariable("id") String artistId) {
-	
+@GetMapping("/view/{id}")
+//@ResponseBody
+public String viewArtist(Model model, @PathVariable("id") String artistId, @ModelAttribute("newReview") Review newReview) {
+	ApiService.clientCredentials_Async();
+	Artist selected = api.findArtistById(artistId);
+	Paging<AlbumSimplified> albums = api.findAlbums(artistId);
+	model.addAttribute("albums", albums);
+	model.addAttribute("artist", selected);
 	return "viewArtist.jsp";
 }
 	
