89ceff4bffb8502a85816e52182ea743f8816dca
Dana Hedrick <dana.marie.hedrick@gmail.com>
2019-10-07 10:55:31 -0700

    added ignore to properties

---
+++ src/main/java/com/music_map/services/ApiService.java
@@ -0,0 +1,12 @@
+package com.music_map.services;
+
+import com.wrapper.spotify.SpotifyApi;
+import com.wrapper.spotify.SpotifyApi.Builder;
+
+public class ApiService {
+
+	public void searchArtist(String a) {
+		final Builder spotifyApi = new SpotifyApi.Builder();
+	}
+	
+}
