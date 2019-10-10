002d94a8128a2707feaf42d5c09af32a751b5ad9
Dana Hedrick <dana.marie.hedrick@gmail.com>
2019-10-10 08:21:14 -0700

    added explore

--- src/main/resources/static/script/explore.js
+++ src/main/resources/static/script/explore.js
@@ -1,3 +1,134 @@
+
 $(function(){
-	let treemap = d3.treemap();
+	let id = "5H0YoDsPDi9fObFmJtTjfN";
+ console.log("id: " + id);
+ grabRelated(id); 
 })
+	
+	
+	function grabRelated(id){
+	$.ajax({
+		  method: "GET",
+		  url: "/artist/api/"+id+"",
+		  contentType:false,
+		  success: function( data ) {
+			  let related = data;
+			  console.log(related);
+			  let length = related.length;
+			  console.log("length"+ length);
+			  let children = [];
+			  for(let i = 0; i < related.length; i++){
+				  children.push({
+					  "name":related[i].name,
+					  "id": related[i].id,
+					  "pic":related[i].images[0].url,
+						"value": 20,
+						"level":"green",
+						"type":"grey",
+				  },)
+			  }
+			  let treeData = [
+					{		
+					"name":"tempname",
+					"id":"5H0YoDsPDi9fObFmJtTjfN",
+					  "pic":related[0].images[0].url,
+
+					"value": 100,
+					"level":"green",
+					"type":"grey",
+					"children":children
+					}
+				]	
+			  console.log(treeData);
+			  drawChart(treeData);
+			  return;
+			  },
+			  error: function(e){
+				  console.log("error"+e);
+			  }
+		  });
+	}
+	
+	function drawChart(treeData){
+		console.log(treeData);
+		const margin = {top: 20, right: 90, bottom: 30, left: 90},
+	      width  = 900 - margin.left - margin.right,
+	      height = 1400 - margin.top - margin.bottom;
+
+	// declares a tree layout and assigns the size
+	const treemap = d3.tree().size([height, width]);
+
+	// assigns the data to a hierarchy using parent-child relationships
+	let nodes = d3.hierarchy(treeData[0], d => d.children);
+
+	// maps the node data to the tree layout
+	nodes = treemap(nodes);
+
+	// append the svg object to the body of the page
+	// appends a 'group' element to 'svg'
+	// moves the 'group' element to the top left margin
+	const svg = d3.select("body").append("svg")
+	        .attr("width", width + margin.left + margin.right)
+	        .attr("height", height + margin.top + margin.bottom),
+	      g = svg.append("g")
+	        // .attr("transform","translate(" + margin.left + "," + margin.top +
+			// ")");
+
+	// adds the links between the nodes
+	const link = g.selectAll(".link")
+	    .data( nodes.descendants().slice(1))
+	  .enter().append("path")
+	    .attr("class", "link")
+	    .style("stroke", d => d.data.level)
+	    .attr("d", d => {
+	       return "M" + d.y + "," + d.x
+	         + "C" + (d.y + d.parent.y) / 2 + "," + d.x
+	         + " " + (d.y + d.parent.y) / 2 + "," + d.parent.x
+	         + " " + d.parent.y + "," + d.parent.x;
+	       });
+
+	// adds each node as a group
+	const node = g.selectAll(".node")
+	    .data(nodes.descendants())
+	    .enter().append("g")
+	    .attr("class", d => "node" + (d.children ? " node--internal" : " node--leaf"))
+	    .attr("transform", d => "translate(" + d.y + "," + d.x + ")")
+		.style("width", "rotate(270deg)")
+	.attr("width", d => d.data.value *5)
+	.attr("height", d => d.data.value *5);
+		
+	node.append("defs")
+	.append("defs:clipPath")
+	.attr("id", "circleView")
+	// .style("border-radius", "5px")
+	.append("clipPath:circle")
+	.attr("r", d => d.data.value*3)
+	.style("width", "auto")
+		.style("height", "auto")
+
+
+	node.append("image")
+	// attr("width", "50")
+	.attr("xlink:href", d => d.data.pic)
+		.style("width", "auto")
+		.style("height", "auto")
+		// .attr("object-fit", "cover")
+		// .style("transform", "rotate(270deg)")
+		.attr("clip-path", "url(#circleView)")
+	
+	// node.append("img:circle")
+	  // .attr("r", d => d.data.value)
+	  // .style("stroke", d => d.data.level)
+	  	 .attr("fill", d => "url(#img)")
+	  // .attr("filter",d => "url("+d.data.pic+")" )
+	 	// .attr("r", d => d.data.value)
+	  
+	// adds the text to the node
+/*
+ * node.append("text") .attr("dy", ".35em") .attr("x", d => d.data.value + 5)
+ * .attr("y", d => d.data.value + 5) .style("text-anchor", d => d.children ?
+ * "end" : "start") .text(d => d.data.name);
+ */
+	
+	
+	}
